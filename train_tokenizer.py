27fe33b8109d4cca9aec337acd6278c45969822a
Daulet Toibazar <113344861+daulettoibazar@users.noreply.github.com>
2024-07-21 19:04:34 +0300

    Update train_tokenizer.py

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -6,23 +6,28 @@ from tokenizers.trainers import WordLevelTrainer
 from pathlib import Path
 import glob
 
-
 @ck.command()
-def main():
-    files = glob.glob('/ibex/user/toibazd/InterPro_annotated_genomes/*.txt')
+@ck.argument('directory', type=ck.Path(exists=True, file_okay=False, dir_okay=True), help="Directory containing '.txt' formatted corpus files")
+@ck.option('--vocab-size', type=int, default=545000, help='Size of the vocabulary')
+@ck.option('--output-file', type=str, default='interpro_tokenizer.json', help='Filename to save tokenizer vocabulary')
+def main(directory, vocab_size, output_file):
+    files = glob.glob(str(Path(directory) / '*.txt'))
     tokenizer = Tokenizer(WordLevel())
     tokenizer.pre_tokenizer = WhitespaceSplit()
-    trainer = WordLevelTrainer(vocab_size=545000,
+    trainer = WordLevelTrainer(
+        vocab_size=vocab_size,
         special_tokens=[
             "<s>",
             "<pad>",
             "</s>",
             "<unk>",
             "<mask>"
-        ])
+        ]
+    )
     
     tokenizer.train(files=files, trainer=trainer)
-    tokenizer.save('interpro_tokenizer.json')
+    tokenizer.save(output_file)
+    print(f"Tokenizer saved to {output_file}")
 
 if __name__ == '__main__':
     main()
